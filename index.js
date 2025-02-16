381ebd11f5edc8242cb3857d4130f2a1a78cf15a
raed saidi <raed.saidi@insat.ucar.tn>
2025-02-16 22:25:46 +0100

    last one

--- index.js
+++ index.js
@@ -8,40 +8,31 @@ const difficultyLevels = {
 
 function setRules() {
     const selectedValue = difficulty.value;
-
-    if (selectedValue === 'facile') {
-        return { ...difficultyLevels.facile, number: Math.floor(Math.random() * difficultyLevels.facile.maxNumber + 1) };
-    } else if (selectedValue === 'intermediaire') {
-        return { ...difficultyLevels.intermediaire, number: Math.floor(Math.random() * difficultyLevels.intermediaire.maxNumber + 1) };
-    } else if (selectedValue === 'difficile') {
-        return { ...difficultyLevels.difficile, number: Math.floor(Math.random() * difficultyLevels.difficile.maxNumber + 1) };
-    }
+    const level = difficultyLevels[selectedValue];
+    return {
+        ...level,
+        number: Math.floor(Math.random() * level.maxNumber + 1)
+    };
 }
 
 function playGame() {
     const rule = setRules();
     let attempts = 0;
-    const number = rule.number;
-    const maxAttempts = rule.maxAttempts;
+    const { number, maxAttempts, maxNumber } = rule;
 
     while (attempts < maxAttempts) {
-        const guess = prompt(`Donner un nombre entre 0 et ${rule.maxNumber} | attempts : ${attempts}`);
+        const guess = +prompt(`Donner un nombre entre 0 et ${maxNumber} | attempts : ${attempts}`);
         attempts++;
 
-        if (guess === null) {
-            alert("Game canceled.");
-            break;
-        }
-
-        if (isNaN(guess)) {
+        if (!guess && guess !== 0) {
             alert("Please enter a valid number.");
             continue;
         }
 
-        if (parseInt(guess) === number) {
+        if (guess === number) {
             alert(`Congratulations! You guessed the number in ${attempts} attempts.`);
             return;
-        } else if (parseInt(guess) < number) {
+        } else if (guess < number) {
             alert("Too low! Try again.");
         } else {
             alert("Too high! Try again.");
